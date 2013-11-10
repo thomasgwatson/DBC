ab218aef7928de46ad0e85d3af6ffbbf7b712a43
Apprentice <apprentice@dbc10.local>
2013-11-10 12:32:09 -0800

    Finished exercise. Click me submits AJAX post request. Post request returns JSON object of server side logic. JS takes returned JSON object and manipulates the DOM in the expected fashion.

--- jQuery_ajax/public/js/application.js
+++ jQuery_ajax/public/js/application.js
@@ -5,8 +5,8 @@ $(document).ready(function(){
     getColorPost()
 
   });
-  function preventinator(e){
-    e.preventDefault()
+  function preventinator(event){
+    event.preventDefault()
     console.log("Should have prevented Default")
   }
   function getColorPost(){
@@ -15,7 +15,11 @@ $(document).ready(function(){
       url: '/color'
     }).done(function(response){
       console.log(response)
+      responseHandler(response)
     })
+    function responseHandler(serverData){
+      $('#color_me li:nth-child(' + serverData.cell + ')').css( 'background-color', serverData.color)
+    }
   }
 });
 }
