cdbf8f747b2fa07370b44ee2c2fe914d42eb97b0
Apprentice <apprentice@dbc10.local>
2013-11-09 18:51:22 -0800

    Unwrapped the add Button Listener and it's other functions

--- js-refactor-to-oo 2/application.js
+++ js-refactor-to-oo 2/application.js
@@ -1,13 +1,28 @@
 $(document).ready(function() {
-  $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
-  });
+  addButtonListener();
+  rollButtonListener();
+});
 
+var wat = function(){
+  console.log("WAT")
+}
+
+var addDie = function(){
+  $('.dice').append('<div class="die">0</div>');
+}
+
+var addButtonListener = function(){
+  $('#roller button.add').on('click', function() {
+    wat()
+    addDie()
+  });
+}
+
+var rollButtonListener = function(){
   $('#roller button.roll').on('click', function() {
     $('.die').each(function(k, die) {
       var value = Math.floor((Math.random()*6)+1);
       $(die).text(value);
     });
   });
-});
+}
