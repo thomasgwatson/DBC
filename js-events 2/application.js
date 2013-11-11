159b0c3954bc5c4dd55091cd1fa94f9557e9b08c
Apprentice <apprentice@dbc10.local>
2013-11-10 20:16:02 -0800

    Added a todo via JQuery

--- js-events 2/application.js
+++ js-events 2/application.js
@@ -4,11 +4,15 @@ $(document).ready(function() {
   function bindEvents() {
     // Bind functions which add, remove, and complete todos to the appropriate
     // elements
+    $('button.add').on('click', function(e){
+      var $toDoItem = buildTodo($('.todo').val())
+      $('.todo_list').append($toDoItem)
+    })
   }
 
   //Create functions to add, remove and complete todos
 
-  
+
 
   function buildTodo(todoName) {
     // Creates an jQueryDOMElement from the todoTemplate.
@@ -18,7 +22,7 @@ $(document).ready(function() {
     // Returns the jQueryDOMElement to be used elsewhere.
     return $todo;
   }
-  
+
 
   bindEvents();
 });
