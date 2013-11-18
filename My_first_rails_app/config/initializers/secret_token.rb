a67ae494de89c70a4ab6e5fc516b8f998fccee7b
Apprentice <apprentice@dbc-01.local>
2013-11-17 19:42:10 -0800

    Added 'Rails the Sinatra Way' from https://gist.github.com/keithtom/3f311c392326bc659b54

---
+++ My_first_rails_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LearningRails::Application.config.secret_token = 'b3534012bfafb32d4e5e83a599ebaee89057146449174fedd3a927753f1da94606ba3933baa24ac7877a9198977827935c22646d3bfc57bc2d1f708af1dd1560'
